e9cfc75f6d6ac7928dc5afffc930bf2ba5d3780c
suri092004 <54814232+suri092004@users.noreply.github.com>
2019-12-21 19:46:26 +0100

    Add files via upload

---
+++ automessages/s.lua
@@ -0,0 +1,12 @@
+﻿local messages = {
+    {"#ffcc00 ★ Jesli chciał byś wesprzeć serwer to możesz rzucić grosza ;) : * * "},
+    {"#ffcc00 ★ Serwerowy Discord :*"},
+	{"#ffcc00 ★ Serwerowe Forum :* "},
+}
+
+function messTime ()
+   randomMessages = math.random(1, #messages)
+   outputChatBox(""..messages[randomMessages][1].."", getRootElement(), 255, 255, 255, true )
+end
+
+setTimer(messTime, 300000, 0)
