e8b2780617d06d2b02b6719f5615b6aa899e7447
suri092004 <54814232+suri092004@users.noreply.github.com>
2020-01-15 23:52:42 +0100

    Add files via upload

--- GiveRank/give.lua
+++ GiveRank/give.lua
@@ -1,16 +1,21 @@
-function AddRank(thePlayer,useless,him,rank)
+function AddRank(thePlayer,_,him,rank)
 	local accName = getAccountName(getPlayerAccount (thePlayer))
-		if isObjectInACLGroup("user."..accName, aclGetGroup("DevGroup") ) then
+		if isObjectInACLGroup("user."..accName, aclGetGroup("Admin") ) then
 		
 			if (rank=='mod') then
 				rank='Moderator'
 			end
+			if (rank=='1') then
+				rank='Moderator'
+			end 
 			
+
+
 			local aclGroup = aclGetGroup(rank)
 			
             aclGroupAddObject (aclGroup, "user."..him)
 			
-            outputChatBox("Gracz "..him.." Awansował  na stanowisko "..rank.."'a Gratulacje C:")
+            outputChatBox(accName.." awansował "..him.." na stanowisko "..rank.."'a Gratulacje C:")
 			
 		end
 end
@@ -18,10 +23,35 @@ aclSave ()
 	addCommandHandler("DajRank", AddRank, false)
 	addCommandHandler("GiveRank", AddRank, false)
 	addCommandHandler("Awansuj", AddRank, false)
+	
+	
+	
+
+function RemoveRank(thePlayer,_,him,rank)
+local accName = getAccountName(getPlayerAccount (thePlayer))
+		if isObjectInACLGroup("user."..accName, aclGetGroup("DevGroup") ) then
+		
+			if (rank=='mod') then
+				rank='Moderator'
+			end
+			if (rank=='1') then
+				rank='Moderator'
+			end 
+			
 
 
+			local aclGroup = aclGetGroup(rank)
+			
+            aclGroupRemoveObject (aclGroup, "user."..him)
+			
+            outputChatBox(him.." został zdegradowany z "..rank.." przez "..accName)
+		
+		
+		end
 
 
-
-
-
+end
+aclSave () 
+	addCommandHandler("Degrad", RemoveRank, false)
+	addCommandHandler("UnRank", RemoveRank, false)
+	addCommandHandler("WezRank", RemoveRank, false)
